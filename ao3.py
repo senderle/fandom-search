e411280a3f2e734a4c9cca2864f18dbab44df78e
Scott Enderle <scott.enderle@gmail.com>
2019-04-09 13:19:59 -0400

    create separate search module

--- ao3.py
+++ ao3.py
@@ -6,20 +6,17 @@ import sys
 import json
 import csv
 import random
-import multiprocessing
-import datetime
 import argparse
 import requests
 import collections
-from collections import Counter, defaultdict
-from operator import itemgetter
 from time import sleep
 
 import numpy
 import pandas as pd
-import nearpy
 from Levenshtein import distance as lev_distance
 from bs4 import BeautifulSoup
+
+import search
 
 import spacy
 _SPACY_MODEL = None  # Model to be loaded later, after we know we need it.
@@ -43,41 +40,6 @@ except ImportError:
     emolex = None
     liwc = None
 
-
-# -----------------------------------------------------------------------------
-# Search Script Settings
-# ---------------
-
-# Set N-Gram window size:
-window_size = 10
-window_size = 6
-
-# Set cosine distance matching threshold:
-distance_threshold = 0.25
-distance_threshold = 0.1
-
-# Set approximate nearest neighbor parameters:
-number_of_hashes = 15  # Bigger -> slower (linear), more matches
-hash_dimensions = 14   # Bigger -> faster (???), fewer matches
-
-new_record_structure = {
-    'fields': ['FAN_WORK_FILENAME',
-               'FAN_WORK_WORD_INDEX',
-               'FAN_WORK_WORD',
-               'FAN_WORK_ORTH_ID',
-               'ORIGINAL_SCRIPT_WORD_INDEX',
-               'ORIGINAL_SCRIPT_WORD',
-               'ORIGINAL_SCRIPT_ORTH_ID',
-               'ORIGINAL_SCRIPT_CHARACTER',
-               'ORIGINAL_SCRIPT_SCENE',
-               'BEST_MATCH_DISTANCE',
-               'BEST_LEVENSHTEIN_DISTANCE',
-               'BEST_COMBINED_DISTANCE',
-              ],
-    'types': [str, int, str, int, int, str,
-              int, str, int, float, int, float
-             ]
-}
 
 # -----------------------------------------------------------------------------
 # HTML TO TXT FUNCTIONS
@@ -183,286 +145,6 @@ def collect_meta(io):
         wr.writeheader()
         for row in rows:
             wr.writerow(row)
-
-# -----------------
-# Utility functions
-# -----------------
-
-def sp_parse_chunks(txt, size=100000):
-    start = 0
-    if len(txt) < 100000:
-        yield _SPACY_MODEL(txt)
-        return
-
-    while start < len(txt):
-        end = start + 100000
-        if end > len(txt):
-            end = len(txt)
-        else:
-            while txt[end] != ' ':
-                end -= 1
-        yield _SPACY_MODEL(txt[start: end])
-        start = end + 1
-
-def mk_vectors(sp_txt):
-    # Given a text, parse it into `spacy`'s native format,
-    # and produce a sequence of vectors, one per token.
-
-    rows = len(sp_txt)
-    cols = len(sp_txt[0].vector) if rows else 0
-
-    vectors = numpy.empty((rows, cols), dtype=float)
-    for i, word in enumerate(sp_txt):
-        if word.has_vector:
-            vectors[i] = word.vector
-        else:
-            # `spacy` doesn't have a pre-trained vector for this word,
-            # so give it a unique random vector.
-            w_str = str(word)
-            vectors[i] = 0
-            vectors[i][hash(w_str) % cols] = 1.0
-            vectors[i][hash(w_str * 2) % cols] = 1.0
-            vectors[i][hash(w_str * 3) % cols] = 1.0
-    return vectors
-
-def build_lsh_engine(orig, window_size, number_of_hashes, hash_dimensions):
-    # Build the ngram vectors using rolling windows.
-    # Variables named `*_win_vectors` contain vectors for
-    # the given input, such that each row is the vector
-    # for a single window. Successive windows overlap
-    # at all words except for the first and last.
-
-    orig_vectors = mk_vectors(orig)
-    orig_win_vectors = numpy.array([orig_vectors[i:i + window_size, :].ravel()
-                                   for i in range(orig_vectors.shape[0] - window_size + 1)])
-
-    # Initialize the approximate nearest neighbor search algorithm.
-    # This creates the search "engine" and populates its index with
-    # the window-vectors from the original script. We can then pass
-    # over the window-vectors from a fan work, taking each vector
-    # and searching for good matches in the engine's index of script
-    # text.
-
-    # We could do the search in the opposite direction, storing
-    # fan text in the engine's index, and passing over window-
-    # vectors from the original script, searching for matches in
-    # the index of fan text. Unfortuantely, the quality of the
-    # matches found goes down when you add too many values to the
-    # engine's index.
-    vector_dim = orig_win_vectors.shape[1]
-
-    hashes = []
-    for i in range(number_of_hashes):
-        h = nearpy.hashes.RandomBinaryProjections('rbp{}'.format(i),
-                                                  hash_dimensions)
-        hashes.append(h)
-
-    engine = nearpy.Engine(vector_dim,
-                           lshashes=hashes,
-                           distance=nearpy.distances.CosineDistance())
-
-    for ix, row in enumerate(orig_win_vectors):
-        engine.store_vector(row, (ix, str(orig[ix: ix + window_size])))
-    return engine
-
-def multi_search_wrapper(work):
-    result = _ANN_INDEX.search(work)
-    return result
-
-class AnnIndexSearch(object):
-    def __init__(self, original_script_filename, window_size,
-                 number_of_hashes, hash_dimensions, distance_threshold):
-        orig_csv = load_markup_script(original_script_filename)
-        orig_csv = orig_csv[1:]  # drop header
-        orig_csv = [[i] + r for i, r in enumerate(orig_csv)]
-        # [['ORIGINAL_SCRIPT_INDEX',
-        #   'LOWERCASE',
-        #   'SPACY_ORTH_ID',
-        #   'SCENE',
-        #   'CHARACTER']]
-
-        (self.word_index,
-         self.word_lowercase,
-         self.orth_id,
-         self.scene,
-         self.character) = zip(*orig_csv)
-
-        self.window_size = window_size
-        self.distance_threshold = distance_threshold
-        orig_doc = spacy.tokens.Doc(_SPACY_MODEL.vocab, self.word_lowercase)
-        self.engine = build_lsh_engine(orig_doc, window_size,
-                                       number_of_hashes, hash_dimensions)
-        self.reset_stats()
-
-    def reset_stats(self):
-        self._windows_processed = 0
-
-    @property
-    def windows_processed(self):
-        return self._windows_processed
-
-    def search(self, filename):
-        with open(filename, encoding='utf8') as fan_file:
-            fan = fan_file.read()
-            fan = [t for ch in sp_parse_chunks(fan) for t in ch]
-
-        # Create the fan windows:
-        fan_vectors = mk_vectors(fan)
-        fan_win_vectors = numpy.array(
-            [fan_vectors[i:i + self.window_size, :].ravel()
-             for i in range(fan_vectors.shape[0] - self.window_size + 1)]
-        )
-
-        duplicate_records = defaultdict(list)
-        for fan_ix, row in enumerate(fan_win_vectors):
-            self._windows_processed += 1
-            results = self.engine.neighbours(row)
-
-            # Extract data about the original script
-            # embedded in the engine's results.
-            results = [(match_ix, match_str, distance)
-                       for vec, (match_ix, match_str), distance in results
-                       if distance < self.distance_threshold]
-
-            # Create a new record with original script
-            # information and fan work information.
-            for match_ix, match_str, distance in results:
-                fan_context = str(fan[fan_ix: fan_ix + window_size])
-                lev_d = lev_distance(match_str, fan_context)
-
-                for window_ix in range(window_size):
-                    fan_word_ix = fan_ix + window_ix
-                    fan_word = fan[fan_word_ix].orth_
-                    fan_orth_id = fan[fan_word_ix].orth
-
-                    orig_word_ix = match_ix + window_ix
-                    orig_word = self.word_lowercase[orig_word_ix]
-                    orig_orth_id = self.orth_id[orig_word_ix]
-                    char = self.character[orig_word_ix]
-                    scene = self.scene[orig_word_ix]
-
-                    duplicate_records[(filename, fan_word_ix)].append(
-                        # NOTE: This **must** match the definition
-                        #       of `record_structure` above
-                        [filename,
-                         fan_word_ix,
-                         fan_word,
-                         fan_orth_id,
-                         orig_word_ix,
-                         orig_word,
-                         orig_orth_id,
-                         char,
-                         scene,
-                         distance,
-                         lev_d,
-                         distance * lev_d]
-                    )
-
-        # To deduplicate duplicate_records, we
-        # pick the single best match, as measured by
-        # the combined distance for the given n-gram
-        # match that first identified the word.
-        for k, dset in duplicate_records.items():
-            duplicate_records[k] = min(dset, key=itemgetter(11))
-        return sorted(duplicate_records.values())
-
-def load_markup_script(filename,
-                        _line_rex=re.compile('LINE<<(?P<line>[^>]*)>>'),
-                        _scene_rex=re.compile('SCENE_NUMBER<<(?P<scene>[^>]*)>>'),
-                        _char_rex=re.compile('CHARACTER_NAME<<(?P<character>[^>]*)>>')):
-    with open(filename, encoding='utf-8') as ip:
-        current_scene = None
-        current_scene_count = 0
-        current_scene_error_fix = False
-        current_char = None
-        rows = [['LOWERCASE', 'SPACY_ORTH_ID', 'SCENE', 'CHARACTER']]
-        for i, line in enumerate(ip):
-            if _scene_rex.search(line):
-                current_scene_count += 1
-                scene_string = _scene_rex.search(line).group('scene')
-                scene_string = ''.join(c for c in scene_string
-                                       if c.isdigit())
-                try:
-                    scene_int = int(scene_string)
-                    current_scene = scene_int
-                except ValueError:
-                    current_scene_error_fix = True
-                    print("Error in Scene markup: {}".format(line))
-
-                if current_scene_error_fix:
-                    current_scene = current_scene_count
-
-            elif _char_rex.search(line):
-                current_char = _char_rex.search(line).group('character')
-            elif _line_rex.search(line):
-                tokens = _SPACY_MODEL(_line_rex.search(line).group('line'))
-                for t in tokens:
-                    # original Spacy lexeme object can be recreated using
-                    #     spacy.lexeme.Lexeme(_SPACY_MODEL.vocab, t.orth)
-                    # where `_SPACY_MODEL = spacy.load('en')`
-                    row = [t.lower_, t.lower, current_scene, current_char]
-                    rows.append(row)
-    return rows
-
-def write_records(records, filename):
-    with open(filename, 'w', encoding='utf-8') as out:
-        wr = csv.writer(out)
-        wr.writerows(records)
-
-def analyze(inputs):
-    fan_work_directory = inputs['d']
-    original_script_markup = inputs['s']
-
-    fan_works = os.listdir(fan_work_directory)
-    fan_works = [os.path.join(fan_work_directory, f)
-                 for f in fan_works]
-
-    # This will always generate the same "random" sample.
-    random.seed(4815162342)
-    random.shuffle(fan_works)
-
-    # cluster_size = 500
-    cluster_size = 500
-    start = 0
-    fan_clusters = [fan_works[i:i + cluster_size]
-                    for i in range(start, len(fan_works), cluster_size)]
-
-    filename_base = 'match-{}gram{{}}'.format(window_size)
-    batch_filename = filename_base.format('-batch-{}.csv')
-
-    accumulated_records = [new_record_structure['fields']]
-    ann_index = AnnIndexSearch(original_script_markup,
-                               window_size,
-                               number_of_hashes,
-                               hash_dimensions,
-                               distance_threshold)
-
-    for i, fan_cluster in enumerate(fan_clusters, start=start):
-        print('Processing cluster {} ({}-{})'.format(i,
-                                                     cluster_size * i,
-                                                     cluster_size * (i + 1)))
-
-        global _ANN_INDEX
-        _ANN_INDEX = ann_index
-        with multiprocessing.Pool(processes=4, maxtasksperchild=10) as pool:
-            chunksize = cluster_size // 25
-            record_sets = pool.map(multi_search_wrapper,
-                                   fan_cluster,
-                                   chunksize=cluster_size // (4 * pool._processes))
-            records = [r for r_set in record_sets for r in r_set]
-            write_records(records, batch_filename.format(i))
-            accumulated_records.extend(records)
-
-    i = 0
-    today_str = '-{:%Y%m%d}.csv'.format(datetime.date.today())
-    name_check = filename_base.format(today_str)
-    while os.path.exists(name_check):
-        i += 1
-        today_str = '-{:%Y%m%d}-{}.csv'.format(datetime.date.today(), i)
-        name_check = filename_base.format(today_str)
-
-    write_records(accumulated_records,
-                  name_check)
 
 #----------------
 #SCRAPE FUNCTIONS
@@ -1045,7 +727,7 @@ if __name__ == '__main__':
     search_parser = subparsers.add_parser('search', help='compare fanworks with the original script')
     search_parser.add_argument('d', action='store', help='directory of fanwork text files')
     search_parser.add_argument('s', action='store', help='filename for markup version of script')
-    search_parser.set_defaults(func=analyze)
+    search_parser.set_defaults(func=search.analyze)
 
     matrix_parser = subparsers.add_parser('matrix', help='deduplicates and builds matrix for best n-gram matches')
     matrix_parser.add_argument('i', action='store', help='input csv file')
@@ -1064,7 +746,7 @@ if __name__ == '__main__':
 
     #call function
     if hasattr(args, 'func'):
-        _SPACY_MODEL = spacy.load('en_core_web_md',
+        search.get_spacy_model() = spacy.load('en_core_web_md',
                                   disable=['parser', 'tagger', 'ner'])
         args.func(vars(args))
     else:
---
+++ search.py
@@ -0,0 +1,330 @@
+
+import multiprocessing
+import datetime
+import csv
+import os
+import random
+from operator import itemgetter
+from collections import defaultdict
+
+import numpy
+import nearpy
+import spacy
+from Levenshtein import distance as lev_distance
+
+_SPACY_MODEL = None
+
+# Approximate nearest neighbors search settings:
+
+new_record_structure = {
+    'fields': ['FAN_WORK_FILENAME',
+               'FAN_WORK_WORD_INDEX',
+               'FAN_WORK_WORD',
+               'FAN_WORK_ORTH_ID',
+               'ORIGINAL_SCRIPT_WORD_INDEX',
+               'ORIGINAL_SCRIPT_WORD',
+               'ORIGINAL_SCRIPT_ORTH_ID',
+               'ORIGINAL_SCRIPT_CHARACTER',
+               'ORIGINAL_SCRIPT_SCENE',
+               'BEST_MATCH_DISTANCE',
+               'BEST_LEVENSHTEIN_DISTANCE',
+               'BEST_COMBINED_DISTANCE',
+               ],
+    'types': [str, int, str, int, int, str,
+              int, str, int, float, int, float
+              ]
+}
+
+
+def get_spacy_model():
+    global _SPACY_MODEL
+    if _SPACY_MODEL is None:
+        _SPACY_MODEL = spacy.load('en_core_web_md',
+                                  disable=['parser', 'tagger', 'ner'])
+    return _SPACY_MODEL
+
+def sp_parse_chunks(txt, size=100000):
+    spacy_model = get_spacy_model()
+
+    start = 0
+    if len(txt) < 100000:
+        yield spacy_model(txt)
+        return
+
+    while start < len(txt):
+        end = start + 100000
+        if end > len(txt):
+            end = len(txt)
+        else:
+            while txt[end] != ' ':
+                end -= 1
+        yield spacy_model(txt[start: end])
+        start = end + 1
+
+def mk_vectors(sp_txt):
+    # Given a text, parse it into `spacy`'s native format,
+    # and produce a sequence of vectors, one per token.
+
+    rows = len(sp_txt)
+    cols = len(sp_txt[0].vector) if rows else 0
+
+    vectors = numpy.empty((rows, cols), dtype=float)
+    for i, word in enumerate(sp_txt):
+        if word.has_vector:
+            vectors[i] = word.vector
+        else:
+            # `spacy` doesn't have a pre-trained vector for this word,
+            # so give it a unique random vector.
+            w_str = str(word)
+            vectors[i] = 0
+            vectors[i][hash(w_str) % cols] = 1.0
+            vectors[i][hash(w_str * 2) % cols] = 1.0
+            vectors[i][hash(w_str * 3) % cols] = 1.0
+    return vectors
+
+def build_lsh_engine(orig, window_size, number_of_hashes, hash_dimensions):
+    # Build the ngram vectors using rolling windows.
+    # Variables named `*_win_vectors` contain vectors for
+    # the given input, such that each row is the vector
+    # for a single window. Successive windows overlap
+    # at all words except for the first and last.
+
+    orig_vectors = mk_vectors(orig)
+    orig_win_vectors = numpy.array([orig_vectors[i:i + window_size, :].ravel()
+                                   for i in range(orig_vectors.shape[0] - window_size + 1)])
+
+    # Initialize the approximate nearest neighbor search algorithm.
+    # This creates the search "engine" and populates its index with
+    # the window-vectors from the original script. We can then pass
+    # over the window-vectors from a fan work, taking each vector
+    # and searching for good matches in the engine's index of script
+    # text.
+
+    # We could do the search in the opposite direction, storing
+    # fan text in the engine's index, and passing over window-
+    # vectors from the original script, searching for matches in
+    # the index of fan text. Unfortuantely, the quality of the
+    # matches found goes down when you add too many values to the
+    # engine's index.
+    vector_dim = orig_win_vectors.shape[1]
+
+    hashes = []
+    for i in range(number_of_hashes):
+        h = nearpy.hashes.RandomBinaryProjections('rbp{}'.format(i),
+                                                  hash_dimensions)
+        hashes.append(h)
+
+    engine = nearpy.Engine(vector_dim,
+                           lshashes=hashes,
+                           distance=nearpy.distances.CosineDistance())
+
+    for ix, row in enumerate(orig_win_vectors):
+        engine.store_vector(row, (ix, str(orig[ix: ix + window_size])))
+    return engine
+
+def multi_search_wrapper(work):
+    result = _ANN_INDEX.search(work)
+    return result
+
+class AnnIndexSearch(object):
+    def __init__(self, original_script_filename, window_size,
+                 number_of_hashes, hash_dimensions, distance_threshold):
+        orig_csv = load_markup_script(original_script_filename)
+        orig_csv = orig_csv[1:]  # drop header
+        orig_csv = [[i] + r for i, r in enumerate(orig_csv)]
+        # [['ORIGINAL_SCRIPT_INDEX',
+        #   'LOWERCASE',
+        #   'SPACY_ORTH_ID',
+        #   'SCENE',
+        #   'CHARACTER']]
+
+        (self.word_index,
+         self.word_lowercase,
+         self.orth_id,
+         self.scene,
+         self.character) = zip(*orig_csv)
+
+        self.window_size = window_size
+        self.distance_threshold = distance_threshold
+        self.spacy_model = get_spacy_model()
+        orig_doc = spacy.tokens.Doc(self.spacy_model.vocab, self.word_lowercase)
+        self.engine = build_lsh_engine(orig_doc, window_size,
+                                       number_of_hashes, hash_dimensions)
+        self.reset_stats()
+
+    def reset_stats(self):
+        self._windows_processed = 0
+
+    @property
+    def windows_processed(self):
+        return self._windows_processed
+
+    def search(self, filename):
+        with open(filename, encoding='utf8') as fan_file:
+            fan = fan_file.read()
+            fan = [t for ch in sp_parse_chunks(fan) for t in ch]
+
+        # Create the fan windows:
+        fan_vectors = mk_vectors(fan)
+        fan_win_vectors = numpy.array(
+            [fan_vectors[i:i + self.window_size, :].ravel()
+             for i in range(fan_vectors.shape[0] - self.window_size + 1)]
+        )
+
+        duplicate_records = defaultdict(list)
+        for fan_ix, row in enumerate(fan_win_vectors):
+            self._windows_processed += 1
+            results = self.engine.neighbours(row)
+
+            # Extract data about the original script
+            # embedded in the engine's results.
+            results = [(match_ix, match_str, distance)
+                       for vec, (match_ix, match_str), distance in results
+                       if distance < self.distance_threshold]
+
+            # Create a new record with original script
+            # information and fan work information.
+            for match_ix, match_str, distance in results:
+                fan_context = str(fan[fan_ix: fan_ix + window_size])
+                lev_d = lev_distance(match_str, fan_context)
+
+                for window_ix in range(window_size):
+                    fan_word_ix = fan_ix + window_ix
+                    fan_word = fan[fan_word_ix].orth_
+                    fan_orth_id = fan[fan_word_ix].orth
+
+                    orig_word_ix = match_ix + window_ix
+                    orig_word = self.word_lowercase[orig_word_ix]
+                    orig_orth_id = self.orth_id[orig_word_ix]
+                    char = self.character[orig_word_ix]
+                    scene = self.scene[orig_word_ix]
+
+                    duplicate_records[(filename, fan_word_ix)].append(
+                        # NOTE: This **must** match the definition
+                        #       of `record_structure` above
+                        [filename,
+                         fan_word_ix,
+                         fan_word,
+                         fan_orth_id,
+                         orig_word_ix,
+                         orig_word,
+                         orig_orth_id,
+                         char,
+                         scene,
+                         distance,
+                         lev_d,
+                         distance * lev_d]
+                    )
+
+        # To deduplicate duplicate_records, we
+        # pick the single best match, as measured by
+        # the combined distance for the given n-gram
+        # match that first identified the word.
+        for k, dset in duplicate_records.items():
+            duplicate_records[k] = min(dset, key=itemgetter(11))
+        return sorted(duplicate_records.values())
+
+def load_markup_script(filename,
+                        _line_rex=re.compile('LINE<<(?P<line>[^>]*)>>'),
+                        _scene_rex=re.compile('SCENE_NUMBER<<(?P<scene>[^>]*)>>'),
+                        _char_rex=re.compile('CHARACTER_NAME<<(?P<character>[^>]*)>>')):
+    with open(filename, encoding='utf-8') as ip:
+        spacy_model = get_spacy_model()
+
+        current_scene = None
+        current_scene_count = 0
+        current_scene_error_fix = False
+        current_char = None
+        rows = [['LOWERCASE', 'SPACY_ORTH_ID', 'SCENE', 'CHARACTER']]
+        for i, line in enumerate(ip):
+            if _scene_rex.search(line):
+                current_scene_count += 1
+                scene_string = _scene_rex.search(line).group('scene')
+                scene_string = ''.join(c for c in scene_string
+                                       if c.isdigit())
+                try:
+                    scene_int = int(scene_string)
+                    current_scene = scene_int
+                except ValueError:
+                    current_scene_error_fix = True
+                    print("Error in Scene markup: {}".format(line))
+
+                if current_scene_error_fix:
+                    current_scene = current_scene_count
+
+            elif _char_rex.search(line):
+                current_char = _char_rex.search(line).group('character')
+            elif _line_rex.search(line):
+                tokens = spacy_model(_line_rex.search(line).group('line'))
+                for t in tokens:
+                    # original Spacy lexeme object can be recreated using
+                    #     spacy.lexeme.Lexeme(get_spacy_model().vocab, t.orth)
+                    row = [t.lower_, t.lower, current_scene, current_char]
+                    rows.append(row)
+    return rows
+
+def write_records(records, filename):
+    with open(filename, 'w', encoding='utf-8') as out:
+        wr = csv.writer(out)
+        wr.writerows(records)
+
+def analyze(inputs,
+            window_size=6,
+            number_of_hashes=15,  # Bigger -> slower (linear), more matches
+            hash_dimensions=14,   # Bigger -> faster (???), fewer matches
+            distance_threshold=0.1,
+            chunk_size=500
+            ):
+    fan_work_directory = inputs['d']
+    original_script_markup = inputs['s']
+
+    fan_works = os.listdir(fan_work_directory)
+    fan_works = [os.path.join(fan_work_directory, f)
+                 for f in fan_works]
+
+    # This will always generate the same "random" sample.
+    random.seed(4815162342)
+    random.shuffle(fan_works)
+
+    start = 0
+    fan_clusters = [fan_works[i:i + chunk_size]
+                    for i in range(start, len(fan_works), chunk_size)]
+
+    filename_base = 'match-{}gram{{}}'.format(window_size)
+    batch_filename = filename_base.format('-batch-{}.csv')
+
+    accumulated_records = [new_record_structure['fields']]
+    ann_index = AnnIndexSearch(original_script_markup,
+                               window_size,
+                               number_of_hashes,
+                               hash_dimensions,
+                               distance_threshold)
+
+    for i, fan_cluster in enumerate(fan_clusters, start=start):
+        print('Processing cluster {} ({}-{})'.format(i,
+                                                     chunk_size * i,
+                                                     chunk_size * (i + 1)))
+
+        global _ANN_INDEX
+        _ANN_INDEX = ann_index
+        with multiprocessing.Pool(processes=4, maxtasksperchild=10) as pool:
+            record_sets = pool.map(
+                multi_search_wrapper,
+                fan_cluster,
+                chunksize=chunk_size // (4 * pool._processes))
+            records = [r for r_set in record_sets for r in r_set]
+            write_records(records, batch_filename.format(i))
+            accumulated_records.extend(records)
+
+    i = 0
+    today_str = '-{:%Y%m%d}.csv'.format(datetime.date.today())
+    name_check = filename_base.format(today_str)
+    while os.path.exists(name_check):
+        i += 1
+        today_str = '-{:%Y%m%d}-{}.csv'.format(datetime.date.today(), i)
+        name_check = filename_base.format(today_str)
+
+    write_records(accumulated_records,
+                  name_check)
+
+
